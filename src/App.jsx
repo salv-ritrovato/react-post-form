99faf32636039ea254e01b0a6f52558dd2524491
salv-ritrovato <salv.ritrovato@gmail.com>
2026-03-18 13:48:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import axios from 'axios';
+
+function App() {
+
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default App
